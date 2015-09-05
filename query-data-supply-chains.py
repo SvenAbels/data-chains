e48e4b5ec677a410cce0caede2d3d460f8dcc1e4
Stefano Bertolo <stefano.bertolo@gmail.com>
2015-09-05 19:35:58 +0200

    minor cleanup

--- query-data-supply-chains.py
+++ query-data-supply-chains.py
@@ -2,7 +2,7 @@ from rdflib.graph import Graph
 from rdflib.namespace import Namespace
 from rdflib.plugins.sparql import prepareQuery
 
-NSDICT = { "schema": Namespace("http://schema.org")}
+NSDICT = {"schema": Namespace("http://schema.org")}
 
 KB_LOCATION = "https://raw.githubusercontent.com/sclopit/data-chains/master/eu-data-supply-chains.html"
 KB_FORMAT = 'rdfa'
@@ -13,11 +13,14 @@ g = Graph()
 g.parse(KB_LOCATION, format=KB_FORMAT)
 
 
-# A list of prepared queries to extract various types of information from the graph.
+# A list of prepared queries to extract various types of information
+# from the graph.
 
 all_query = prepareQuery('SELECT DISTINCT ?s ?p ?o WHERE {?s ?p ?o .}')
 
-buyers_query = prepareQuery('SELECT DISTINCT ?buyer WHERE { ?sale schema:agent ?buyer .}', initNs = NSDICT)
+buyers_query = prepareQuery(
+    'SELECT DISTINCT ?buyer WHERE { ?sale schema:agent ?buyer .}',
+    initNs=NSDICT)
 
 # running one of the queries and displaying the results
 
